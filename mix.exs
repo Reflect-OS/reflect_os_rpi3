f0840ae794316c6e2bb3673e11fbce3b32666f2c
Drew Trafton <dtraft@gmail.com>
2024-10-24 21:02:45 -0400

    Initial commit

--- mix.exs
+++ mix.exs
@@ -1,8 +1,8 @@
 defmodule NervesSystemRpi3.MixProject do
   use Mix.Project
 
-  @github_organization "nerves-project"
-  @app :nerves_system_rpi3
+  @github_organization "Reflect-OS"
+  @app :reflect_os_rpi3
   @source_url "https://github.com/#{@github_organization}/#{@app}"
   @version Path.join(__DIR__, "VERSION")
            |> File.read!()
@@ -77,7 +77,7 @@ defmodule NervesSystemRpi3.MixProject do
 
   defp description do
     """
-    Nerves System - Raspberry Pi 3B, 3B+, Zero 2W
+    ReflectOS - Raspberry Pi 3B, 3B+, Zero 2W
     """
   end
 
